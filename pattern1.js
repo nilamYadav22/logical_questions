26540652b4847e2f160b6ceacc20ba5de483ed8a
nilamYadav22 <nilamindia18@gmail.com>
2022-08-08 16:24:18 +0530

    LogicalQuestions

---
+++ pattern1.js
@@ -0,0 +1,123 @@
+// for(var i=1;i<10;i++){
+//   for(var j=1;j<=i;j++){
+//     console.log(j,end = ' ');
+
+//       }
+//       }
+
+// var i;
+// for(i=10; i>=0; i= i-1){
+//    var s;
+//    for(s=0; s<i; s = s+1){
+//     console("*");
+//    }
+//    //i want this to print a new line
+//    ('\n');
+
+// }
+
+// for(var i = 0; i <= 5 ; i++) {
+//     for(var j = 1; j <= i; j++) {
+//         console.log (i);
+
+//     }
+//     console.log(' ');
+        
+        
+
+// }
+// console.log(i);
+
+// var string=""
+// for(var i = 1; i <= 5 ; i++) {
+//     for(var j = 1; j <= i; j++) {
+//       string+=j
+    
+    
+//     }
+    
+//     string+="\n"
+    
+// }
+// console.log(string);
+// OutPut
+// 1
+// 12
+// 123
+// 1234
+// 12345
+
+var string=""
+for(var i = 1; i <= 5 ; i++) {
+    for(var j = 1; j <= i; j++) {
+      string+=i
+    
+    
+    }
+    
+    string+="\n"
+    
+}
+console.log(string);
+// OutPut
+// 1
+// 22
+// 333
+// 4444
+// 55555
+
+// var string=""
+// for(var i = 6; i >=1 ; i--) {
+//     for(var j = 6; j >= i; j--) {
+//       string+=i
+    
+    
+//     }
+    
+//     string+="\n"
+    
+// }
+// console.log(string);
+// Output
+// 6
+// 55
+// 444
+// 3333
+// 22222
+// 111111
+
+// var string=""
+// for(var i = 6; i >=1 ; i--) {
+//     for(var j = 6; j >= i; j--) {
+//       string+=i
+    
+    
+//     }
+//     string+="\n"
+ 
+    
+// }  
+
+// console.log(string);
+
+// var string=""
+// for(var i = 6; i >=1 ; i--) {
+//     for(var j = 6; j >= i; j--) {
+//       string+="*"
+      
+    
+    
+//     }
+//     string+="\n"
+ 
+    
+// }  
+
+// console.log(string);
+
+
+
+
+
+
+
